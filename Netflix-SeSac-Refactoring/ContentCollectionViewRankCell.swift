aea17540b74f8822cbbb40da26bd5b10ba5a1f4c
im667 <koh9037@gmail.com>
2022-04-17 23:05:27 +0900

    ADD: Content

---
+++ Netflix-SeSac-Refactoring/ContentCollectionViewRankCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContentCollectionViewRankCell.swift
+//  Netflix-SeSac-Refactoring
+//
+//  Created by mac on 2022/04/14.
+//
+
+
+import UIKit
+
+class ContentCollectionViewRankCell: UICollectionViewCell {
+    
+    
+}
